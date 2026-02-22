3ed00b769c7c7a6d63a0229b8739a97fcb863dd4
leak <123011509+a9f3c2e8@users.noreply.github.com>
2026-02-22 22:15:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_ID = int(os.getenv('ADMIN_ID', 0))
 ADMIN_IDS = [ADMIN_ID, 947618200]  # Список всех админов
-DB_PATH = 'doublekiss.db'
+DB_PATH = '/app/doublekiss.db'
